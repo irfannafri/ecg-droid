419a1430bfa48f28ecb573f9ddb19dd76022dd19
Irfan <irfanlangang@gmail.com>
2018-02-19 13:34:34 +0700

    upload record to server feature

--- app/src/main/java/com/ilham1012/ecgbpi/activity/DashboardNewActivity.java
+++ app/src/main/java/com/ilham1012/ecgbpi/activity/DashboardNewActivity.java
@@ -7,7 +7,7 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
+//import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -22,6 +22,28 @@ import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
 import android.widget.Toast;
 
+
+import android.os.Environment;
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.net.Uri;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import com.github.clans.fab.FloatingActionButton;
+import com.github.clans.fab.FloatingActionMenu;
+
 import com.ilham1012.ecgbpi.R;
 import com.ilham1012.ecgbpi.helper.SQLiteHandler;
 import com.ilham1012.ecgbpi.helper.SessionManager;
@@ -31,6 +53,14 @@ public class DashboardNewActivity extends AppCompatActivity {
     private SessionManager session;
     private ListView listView;
     private SimpleCursorAdapter dataAdapter;
+
+    private static final int PICK_FILE_REQUEST = 1;
+    private static final String TAG = DashboardNewActivity.class.getSimpleName();
+    private String selectedFilePath;
+    private String SERVER_URL = "http://192.168.137.1/ecgdroid/server.php";
+
+    FloatingActionMenu materialDesignFAM;
+    ProgressDialog dialog;
 
     // Constants
     // The authority for the sync adapter's content provider
@@ -50,7 +80,7 @@ public class DashboardNewActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         listView = (ListView) findViewById(R.id.ecgRecordListView);
-
+        materialDesignFAM = (FloatingActionMenu) findViewById(R.id.floatingMenu);
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -62,6 +92,37 @@ public class DashboardNewActivity extends AppCompatActivity {
 
             }
         });
+        FloatingActionButton ivAttachment = (FloatingActionButton) findViewById(R.id.ivAttachment);
+        ivAttachment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showFileChooser();
+            }
+        });
+
+        FloatingActionButton bUpload = (FloatingActionButton) findViewById(R.id.b_upload);
+        bUpload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //on upload button Click
+                if(selectedFilePath != null){
+//                    dialog = ProgressDialog.show(DashboardNewActivity.this,"","Uploading File...",true);
+
+                    new Thread(new Runnable() {
+                        @Override
+                        public void run() {
+                            //creating new thread to handle Http Operations
+                            uploadFile(selectedFilePath);
+                        }
+                    }).start();
+                }else{
+                    Toast.makeText(DashboardNewActivity.this,"Please choose a File First",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
 
         // Sqlite db handler
         db = new SQLiteHandler(getApplicationContext());
@@ -79,7 +140,165 @@ public class DashboardNewActivity extends AppCompatActivity {
         if (!session.isLoggedIn()){
             logoutUser();
         }
+    }
 
+    private void showFileChooser() {
+//        Intent intent = new Intent();
+//        //sets the select file to all types of files
+//        intent.setType("*/*");
+//        //allows to select data and return it
+//        intent.setAction(Intent.ACTION_GET_CONTENT);
+//        //starts new activity to select file and return data
+//        startActivityForResult(Intent.createChooser(intent,"Choose File to Upload.."),PICK_FILE_REQUEST);
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        Uri uri = Uri.parse(Environment.getExternalStorageDirectory().getPath()
+                + "/ecgbpi/ecgrecord/");
+        intent.setDataAndType(uri, "*/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Open folder"),PICK_FILE_REQUEST);
+
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode == Activity.RESULT_OK){
+            if(requestCode == PICK_FILE_REQUEST){
+                if(data == null){
+                    //no data present
+                    return;
+                }
+
+
+                Uri selectedFileUri = data.getData();
+                selectedFilePath = FilePath.getPath(this,selectedFileUri);
+                Log.i(TAG,"Selected File Path:" + selectedFilePath);
+
+                if(selectedFilePath != null && !selectedFilePath.equals("")){
+//                    tvFileName.setText(selectedFilePath);
+                }else{
+                    Toast.makeText(this,"Cannot upload file to server",Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+    }
+    public int uploadFile(final String selectedFilePath){
+
+        int serverResponseCode = 0;
+
+        HttpURLConnection connection;
+        DataOutputStream dataOutputStream;
+        String lineEnd = "\r\n";
+        String twoHyphens = "--";
+        String boundary = "*****";
+
+
+        int bytesRead,bytesAvailable,bufferSize;
+        byte[] buffer;
+        int maxBufferSize = 1 * 1024 * 1024;
+        File selectedFile = new File(selectedFilePath);
+
+
+        String[] parts = selectedFilePath.split("/");
+        final String fileName = parts[parts.length-1];
+
+        if (!selectedFile.isFile()){
+            dialog.dismiss();
+
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+//                    tvFileName.setText("Source File Doesn't Exist: " + selectedFilePath);
+                }
+            });
+            return 0;
+        }else{
+            try{
+                FileInputStream fileInputStream = new FileInputStream(selectedFile);
+                URL url = new URL(SERVER_URL);
+                connection = (HttpURLConnection) url.openConnection();
+                connection.setDoInput(true);//Allow Inputs
+                connection.setDoOutput(true);//Allow Outputs
+                connection.setUseCaches(false);//Don't use a cached Copy
+                connection.setRequestMethod("POST");
+                connection.setRequestProperty("Connection", "Keep-Alive");
+                connection.setRequestProperty("ENCTYPE", "multipart/form-data");
+                connection.setRequestProperty("Content-Type", "multipart/form-data;boundary=" + boundary);
+                connection.setRequestProperty("uploaded_file",selectedFilePath);
+
+                //creating new dataoutputstream
+                dataOutputStream = new DataOutputStream(connection.getOutputStream());
+
+                //writing bytes to data outputstream
+                dataOutputStream.writeBytes(twoHyphens + boundary + lineEnd);
+                dataOutputStream.writeBytes("Content-Disposition: form-data; name=\"uploaded_file\";filename=\""
+                        + selectedFilePath + "\"" + lineEnd);
+
+                dataOutputStream.writeBytes(lineEnd);
+
+                //returns no. of bytes present in fileInputStream
+                bytesAvailable = fileInputStream.available();
+                //selecting the buffer size as minimum of available bytes or 1 MB
+                bufferSize = Math.min(bytesAvailable,maxBufferSize);
+                //setting the buffer as byte array of size of bufferSize
+                buffer = new byte[bufferSize];
+
+                //reads bytes from FileInputStream(from 0th index of buffer to buffersize)
+                bytesRead = fileInputStream.read(buffer,0,bufferSize);
+
+                //loop repeats till bytesRead = -1, i.e., no bytes are left to read
+                while (bytesRead > 0){
+                    //write the bytes read from inputstream
+                    dataOutputStream.write(buffer,0,bufferSize);
+                    bytesAvailable = fileInputStream.available();
+                    bufferSize = Math.min(bytesAvailable,maxBufferSize);
+                    bytesRead = fileInputStream.read(buffer,0,bufferSize);
+                }
+
+                dataOutputStream.writeBytes(lineEnd);
+                dataOutputStream.writeBytes(twoHyphens + boundary + twoHyphens + lineEnd);
+
+                serverResponseCode = connection.getResponseCode();
+                String serverResponseMessage = connection.getResponseMessage();
+
+                Log.i(TAG, "Server Response is: " + serverResponseMessage + ": " + serverResponseCode);
+
+                //response code of 200 indicates the server status OK
+                if(serverResponseCode == 200){
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+//                            tvFileName.setText("File Upload completed.\n"+ fileName);
+                        }
+                    });
+                }
+
+                //closing the input and output streams
+                fileInputStream.close();
+                dataOutputStream.flush();
+                dataOutputStream.close();
+
+
+
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(DashboardNewActivity.this,"File Not Found",Toast.LENGTH_SHORT).show();
+                    }
+                });
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+                Toast.makeText(DashboardNewActivity.this, "URL error!", Toast.LENGTH_SHORT).show();
+
+            } catch (IOException e) {
+                e.printStackTrace();
+                Toast.makeText(DashboardNewActivity.this, "Cannot Read/Write File!", Toast.LENGTH_SHORT).show();
+            }
+            dialog.dismiss();
+//            Toast.makeText(DashboardNewActivity.this,"Belum konek mang",Toast.LENGTH_SHORT).show();
+            return serverResponseCode;
+        }
 
     }
 
